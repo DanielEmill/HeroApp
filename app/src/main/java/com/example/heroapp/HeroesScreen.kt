2cf075ff361267285330bfe821dd5e249bdcf555
Emill Daniel Garcia Castellanos <Danielemill2012@gmail.com>
2023-08-11 22:50:24 -0400

    Finish

--- app/src/main/java/com/example/heroapp/HeroesScreen.kt
+++ app/src/main/java/com/example/heroapp/HeroesScreen.kt
@@ -23,13 +23,10 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
@@ -38,11 +35,52 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.heroapp.Model.Hero
-import com.example.heroapp.Model.HeroesRepository
 import com.example.heroapp.ui.theme.HeroAppTheme
-
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
-fun HeroListItem(hero: Hero, modifier: Modifier = Modifier){
+fun HeroesList(
+    heroes: List<Hero>,
+    modifier: Modifier = Modifier,
+) {
+    val visibleState = remember {
+        MutableTransitionState(false).apply {
+            // Start the animation immediately.
+            targetState = true
+        }
+    }
+
+    // Fade in entry animation for the entire list
+    AnimatedVisibility(
+        visibleState = visibleState,
+        enter = fadeIn(
+            animationSpec = spring(dampingRatio = DampingRatioLowBouncy)
+        ),
+        exit = fadeOut(),
+        modifier = modifier
+    ) {
+        LazyColumn {
+            itemsIndexed(heroes) { index, hero ->
+                HeroListItem(
+                    hero = hero,
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp, vertical = 8.dp)
+                        // Animate each list item to slide in vertically
+                        .animateEnterExit(
+                            enter = slideInVertically(
+                                animationSpec = spring(
+                                    stiffness = StiffnessVeryLow,
+                                    dampingRatio = DampingRatioLowBouncy
+                                ),
+                                initialOffsetY = { it * (index + 1) } // staggered entrance
+                            )
+                        )
+                )
+            }
+        }
+    }
+}
+@Composable
+fun HeroListItem(hero: Hero,modifier: Modifier = Modifier){
     Card(modifier = modifier,){
         Row(modifier = Modifier
                 .fillMaxWidth()
@@ -74,15 +112,3 @@ fun HeroListItem(hero: Hero, modifier: Modifier = Modifier){
 
 
 
-@Preview("Dark Theme")
-@Composable
-fun HeroPreview() {
-    val hero = Hero(
-        R.string.hero1,
-        R.string.description1,
-        R.drawable.android_superhero1
-    )
-    HeroAppTheme {
-        HeroListItem(hero = hero)
-    }
-}
